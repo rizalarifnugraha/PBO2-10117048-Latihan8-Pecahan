587f3579614ba1bf4355440e22d0247456a11640
rizalarifnugraha <43487664+rizalarifnugraha@users.noreply.github.com>
2018-09-22 19:51:17 +0700

    Add files via upload

---
+++ src/pbo/pkg10117048/latihan8/pecahan/PBO10117048Latihan8Pecahan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10117048.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ * Nama  : Rizal Arif Nugraha
+ * Kelas : PBO2 
+ * NIM   : 10117048
+ * Deskripsi Program : Program ini berisi program yang menggunakan float dan
+ * double untuk mencari pecahan.  
+ * 
+
+ */
+public class PBO10117048Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       float f;
+       double d;
+       f = 1234567890.123456789F;
+       d = 1_234_567_890.123456789;
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
